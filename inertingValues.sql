e25b9a58de124d581e1243dc5171ea33c31500e0
gnozadi <56971838+gnozadi@users.noreply.github.com>
2023-01-01 12:31:03 +0330

    initial commit - customers

---
+++ inertingValues.sql
@@ -0,0 +1,14 @@
+use myshop;
+
+insert into address(country, city, street, blockNumber, Location, Phone,PostalCode) values('Iran', 'Yazd', 'Kashani', 10,'15.6160034,35.9391429,4z',6712436,13566);
+insert into address(country, city, street, blockNumber, Location, Phone,PostalCode) values('Iran', 'Mashhad', 'Farhad', 5,'15.781368,35.8452369,4z',4254565,75486);
+insert into address(country, city, street, blockNumber, Location, Phone,PostalCode) values('Iran', 'Shiraz', 'Zand', 7,'15.6478234,35.7523468,4z',1025463,96325);
+insert into address(country, city, street, blockNumber, Location, Phone,PostalCode) values('Iran', 'Abadan', 'Amiri', 1,'15.6160034,35.9391429,4z',7845123,78523);
+insert into address(country, city, street, blockNumber, Location, Phone,PostalCode) values('Iran', 'Tehran', 'Enghelab', 15,'15.6160034,35.9391429,4z',6712436,13566);
+insert into address(country, city, street, blockNumber, Location, Phone,PostalCode) values('Iran', 'Ghom', 'Azar', 9,'15.6160034,35.9391429,4z',6712436,13566);
+insert into address(country, city, street, blockNumber, Location, Phone,PostalCode) values('Iran', 'Karaj', 'Aban', 4,'15.6160034,35.9391429,4z',6712436,13566);
+insert into address(country, city, street, blockNumber, Location, Phone,PostalCode) values('Iran', 'Rasht', 'Takhti', 11,'15.6160034,35.9391429,4z',6712436,13566);
+insert into address(country, city, street, blockNumber, Location, Phone,PostalCode) values('Iran', 'Saari', 'Farhang', 16,'15.6160034,35.9391429,4z',6712436,13566);
+insert into address(country, city, street, blockNumber, Location, Phone,PostalCode) values('Iran', 'Babol', 'Beheshti', 11,'15.6160034,35.9391429,4z',6712436,13566);
+insert into address(country, city, street, blockNumber, Location, Phone,PostalCode) values('Iran', 'Sanandaj', 'Ferdowsi', 3,'15.6160034,35.9391429,4z',6712436,13566);
+insert into address(country, city, street, blockNumber, Location, Phone,PostalCode) values('Iran', 'Kerman', 'Jahaad', 2,'15.6160034,35.9391429,4z',6712436,13566);
