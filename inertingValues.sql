4ad398ebc08eac53d8b93496746eff35d3b50ed5
gnozadi <56971838+gnozadi@users.noreply.github.com>
2023-01-01 12:42:21 +0330

    inserting customerProfile

--- inertingValues.sql
+++ inertingValues.sql
@@ -13,5 +13,21 @@ use myshop;
 -- insert into address(country, city, street, blockNumber, Location, Phone,PostalCode) values('Iran', 'Sanandaj', 'Ferdowsi', 3,'15.6160034,35.9391429,4z',6712436,13566);
 -- insert into address(country, city, street, blockNumber, Location, Phone,PostalCode) values('Iran', 'Kerman', 'Jahaad', 2,'15.6160034,35.9391429,4z',6712436,13566);
 
-insert into category(CategoryName) values ('Bedroom');
-insert into category(CategoryName) values ('Bathroom');
+-- insert into category(CategoryName) values ('Bedroom');
+-- insert into category(CategoryName) values ('Bathroom');
+
+
+insert into customerprofile(Username, Userpassword, Email) values('Ali Mohammadi', '1234','a.mohammadi@gmail.com');
+insert into customerprofile(Username, Userpassword, Email) values('Paria Behnam', '1234','PariaBehanam@gmail.com');
+insert into customerprofile(Username, Userpassword, Email) values('Mostafa Moradi', '1234','Moradi.Mostafa@gmail.com');
+insert into customerprofile(Username, Userpassword, Email) values('Rahaa Alipoor', '1234','ralipoor@gmail.com');
+insert into customerprofile(Username, Userpassword, Email) values('Koorosh Samani', '1234','samani.koorosh@gmail.com');
+insert into customerprofile(Username, Userpassword, Email) values('Leila Hosseine', '1234','Leila.hosseini@gmail.com');
+insert into customerprofile(Username, Userpassword, Email) values('Parsa Jahani', '1234','Parsa.Jahani@gmail.com');
+insert into customerprofile(Username, Userpassword, Email) values('Mitra Salari', '1234','m.salari@gmail.com');
+insert into customerprofile(Username, Userpassword, Email) values('Reza Nasiri', '1234','r-nasiri@gmail.com');
+insert into customerprofile(Username, Userpassword, Email) values('Nazanin Alavi', '1234','Nazain.alavi@gmail.com');
+insert into customerprofile(Username, Userpassword, Email) values('Taha Pahlavan', '1234','t.pahlavan@gmail.com');
+insert into customerprofile(Username, Userpassword, Email) values('Reihane Samadi', '1234','r-samadi@gmail.com');
+insert into customerprofile(Username, Userpassword, Email) values('Raamin Rezazade', '1234','Ramin-rezazade@gmail.com');
+insert into customerprofile(Username, Userpassword, Email) values('Elahe Hamidi', '1234','hamidi-elahe@gmail.com');
