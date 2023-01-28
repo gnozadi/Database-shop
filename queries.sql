20ce62fb8a99bfad8c26add5d5d088fc7270cd0d
Mahya Ehsanimehr <64207505+mahya-ehs@users.noreply.github.com>
2023-01-28 12:22:01 +0330

    Revert "adding queries no. 2, 9, 15, 16, 17"
    This reverts commit ba0543d0ce285e3b4026ef217dba8ff27e2f2f6a.

--- queries.sql
+++ queries.sql
@@ -1,29 +1,14 @@
--- query no.2: show users list
-select * from customer;
+use myshop;
+-- order list -- 
+select factor.ID, TotalPrice, TotalDiscount, 'Date' from factor, shoppingcart where FactorID = factor.ID;
+
+-- suggested list -- 
+select * from product where Discount>=15; 
+
+-- 3 least review --
+select ProductName, Brand, Price, product_review.Title, product_review.Rating 
+from product, product_review
+where productID = product.ID && ProductName = "Mug"
+order by Rating limit 3
 
 
--- query no.9: show the most cheap selling provider for admin
-select PName as ProviderName, ProductName, table1.Price as Price from provider, product, (
-	select ProductID, ProviderID, min(Price) as Price
-	from product_provider
-	group by ProductID
-    ) as table1
-where provider.ID = table1.ProviderID and product.ID = table1.ProductID;
-
-   
--- query no.15: show the average sale of the month for admin
-select EXTRACT(month from TransactionDate) as month, avg(TotalPrice) as averageSale from transaction, factor
-where transaction.FactorID = factor.ID
-group by month
-order by month asc;
--- end
-
--- query no.16: show the users from a city
-select City, FullName as CustomerName from customer, address
-where address.ID = customer.AddressID
-order by City;
-
--- query no.17: show the providers from a city
-select City, PName as ProviderName from provider, address
-where address.ID = provider.AddressID
-order by City;
